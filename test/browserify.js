214244c0218dac35210b6025b7d8e54dec42666b
Stefan Penner <stefan.penner@gmail.com>
2016-02-29 16:12:48 -0800

    Release 3.2.1

--- test/browserify.js
+++ test/browserify.js
@@ -10188,7 +10188,7 @@ function hasOwnProperty(obj, prop) {
     var lib$es6$promise$asap$$browserWindow = (typeof window !== 'undefined') ? window : undefined;
     var lib$es6$promise$asap$$browserGlobal = lib$es6$promise$asap$$browserWindow || {};
     var lib$es6$promise$asap$$BrowserMutationObserver = lib$es6$promise$asap$$browserGlobal.MutationObserver || lib$es6$promise$asap$$browserGlobal.WebKitMutationObserver;
-    var lib$es6$promise$asap$$isNode = typeof process !== 'undefined' && {}.toString.call(process) === '[object process]';
+    var lib$es6$promise$asap$$isNode = typeof self === 'undefined' && typeof process !== 'undefined' && {}.toString.call(process) === '[object process]';
 
     // test for web worker but not in IE10
     var lib$es6$promise$asap$$isWorker = typeof Uint8ClampedArray !== 'undefined' &&
@@ -10278,19 +10278,19 @@ function hasOwnProperty(obj, prop) {
     }
     function lib$es6$promise$then$$then(onFulfillment, onRejection) {
       var parent = this;
-      var state = parent._state;
-
-      if (state === lib$es6$promise$$internal$$FULFILLED && !onFulfillment || state === lib$es6$promise$$internal$$REJECTED && !onRejection) {
-        return this;
-      }
 
       var child = new this.constructor(lib$es6$promise$$internal$$noop);
-      var result = parent._result;
+
+      if (child[lib$es6$promise$$internal$$PROMISE_ID] === undefined) {
+        lib$es6$promise$$internal$$makePromise(child);
+      }
+
+      var state = parent._state;
 
       if (state) {
         var callback = arguments[state - 1];
         lib$es6$promise$asap$$asap(function(){
-          lib$es6$promise$$internal$$invokeCallback(state, child, callback, result);
+          lib$es6$promise$$internal$$invokeCallback(state, child, callback, parent._result);
         });
       } else {
         lib$es6$promise$$internal$$subscribe(parent, child, onFulfillment, onRejection);
@@ -10312,6 +10312,7 @@ function hasOwnProperty(obj, prop) {
       return promise;
     }
     var lib$es6$promise$promise$resolve$$default = lib$es6$promise$promise$resolve$$resolve;
+    var lib$es6$promise$$internal$$PROMISE_ID = Math.random().toString(36).substring(16);
 
     function lib$es6$promise$$internal$$noop() {}
 
@@ -10542,6 +10543,18 @@ function hasOwnProperty(obj, prop) {
       }
     }
 
+    var lib$es6$promise$$internal$$id = 0;
+    function lib$es6$promise$$internal$$nextId() {
+      return lib$es6$promise$$internal$$id++;
+    }
+
+    function lib$es6$promise$$internal$$makePromise(promise) {
+      promise[lib$es6$promise$$internal$$PROMISE_ID] = lib$es6$promise$$internal$$id++;
+      promise._state = undefined;
+      promise._result = undefined;
+      promise._subscribers = [];
+    }
+
     function lib$es6$promise$promise$all$$all(entries) {
       return new lib$es6$promise$enumerator$$default(this, entries).promise;
     }
@@ -10550,28 +10563,18 @@ function hasOwnProperty(obj, prop) {
       /*jshint validthis:true */
       var Constructor = this;
 
-      var promise = new Constructor(lib$es6$promise$$internal$$noop);
-
       if (!lib$es6$promise$utils$$isArray(entries)) {
-        lib$es6$promise$$internal$$reject(promise, new TypeError('You must pass an array to race.'));
-        return promise;
+        return new Constructor(function(resolve, reject) {
+          reject(new TypeError('You must pass an array to race.'));
+        });
+      } else {
+        return new Constructor(function(resolve, reject) {
+          var length = entries.length;
+          for (var i = 0; i < length; i++) {
+            Constructor.resolve(entries[i]).then(resolve, reject);
+          }
+        });
       }
-
-      var length = entries.length;
-
-      function onFulfillment(value) {
-        lib$es6$promise$$internal$$resolve(promise, value);
-      }
-
-      function onRejection(reason) {
-        lib$es6$promise$$internal$$reject(promise, reason);
-      }
-
-      for (var i = 0; promise._state === lib$es6$promise$$internal$$PENDING && i < length; i++) {
-        lib$es6$promise$$internal$$subscribe(Constructor.resolve(entries[i]), undefined, onFulfillment, onRejection);
-      }
-
-      return promise;
     }
     var lib$es6$promise$promise$race$$default = lib$es6$promise$promise$race$$race;
     function lib$es6$promise$promise$reject$$reject(reason) {
@@ -10583,7 +10586,6 @@ function hasOwnProperty(obj, prop) {
     }
     var lib$es6$promise$promise$reject$$default = lib$es6$promise$promise$reject$$reject;
 
-    var lib$es6$promise$promise$$counter = 0;
 
     function lib$es6$promise$promise$$needsResolver() {
       throw new TypeError('You must pass a resolver function as the first argument to the promise constructor');
@@ -10698,9 +10700,8 @@ function hasOwnProperty(obj, prop) {
       @constructor
     */
     function lib$es6$promise$promise$$Promise(resolver) {
-      this._id = lib$es6$promise$promise$$counter++;
-      this._state = undefined;
-      this._result = undefined;
+      this[lib$es6$promise$$internal$$PROMISE_ID] = lib$es6$promise$$internal$$nextId();
+      this._result = this._state = undefined;
       this._subscribers = [];
 
       if (lib$es6$promise$$internal$$noop !== resolver) {
@@ -10951,6 +10952,10 @@ function hasOwnProperty(obj, prop) {
       this._instanceConstructor = Constructor;
       this.promise = new Constructor(lib$es6$promise$$internal$$noop);
 
+      if (!this.promise[lib$es6$promise$$internal$$PROMISE_ID]) {
+        lib$es6$promise$$internal$$makePromise(this.promise);
+      }
+
       if (Array.isArray(input)) {
         this._input     = input;
         this.length     = input.length;
@@ -10968,13 +10973,13 @@ function hasOwnProperty(obj, prop) {
           }
         }
       } else {
-        lib$es6$promise$$internal$$reject(this.promise, this._validationError());
+        lib$es6$promise$$internal$$reject(this.promise, lib$es6$promise$enumerator$$validationError());
       }
     }
 
-    lib$es6$promise$enumerator$$Enumerator.prototype._validationError = function() {
+    function lib$es6$promise$enumerator$$validationError() {
       return new Error('Array Methods must be provided an Array');
-    };
+    }
 
     lib$es6$promise$enumerator$$Enumerator.prototype._enumerate = function() {
       var length  = this.length;
@@ -11160,6 +11165,59 @@ describe('tampering', function() {
       });
     });
 
+    describe('alternative constructor', function() {
+      it('tampered resolved and then', function() {
+        var one = Promise.resolve(1);
+        var two = Promise.resolve(2);
+        var thenCalled = 0;
+        var resolveCalled = 0;
+        var invokedAlternativeConstructor = 0;
+
+        two.then = function() {
+          thenCalled++;
+          return Promise.prototype.then.apply(this, arguments);
+        };
+
+        function AlternativeConstructor(executor) {
+          invokedAlternativeConstructor++;
+          var followers = this.followers = [];
+          executor(function(value) {
+            followers.forEach(function(onFulfillment) {
+              onFulfillment(value)
+            });
+          }, function() {
+            throw TypeError('No Rejections supported');
+          });
+        }
+
+        AlternativeConstructor.resolve = function(x) {
+          resolveCalled++;
+          return new Promise(function(resolve) { resolve(x); });
+        };
+
+        AlternativeConstructor.prototype.then = function(onFulfillment, onRejection) {
+          this.followers.push(onFulfillment);
+        };
+
+        AlternativeConstructor.resolve = function(x) {
+          resolveCalled++;
+          return new Promise(function(resolve) { resolve(x); });
+        };
+
+        one.constructor = AlternativeConstructor
+
+        return one.then(function() {
+          return two;
+        }).then(function(value) {
+
+          assert.equal(invokedAlternativeConstructor, 1, 'expected AlternativeConstructor to be invoked once');
+          assert.equal(thenCalled, 1, 'expected then to be called once');
+          assert.equal(resolveCalled, 0, 'expected resolve to be called once');
+          assert.equal(value, 2, 'expected fulfillment value to be 2');
+        });
+      });
+    });
+
     describe('Promise.all', function() {
       it('tampered resolved and then', function() {
         var two = Promise.resolve(2);
@@ -11182,7 +11240,120 @@ describe('tampering', function() {
           assert.deepEqual(value, [2]);
         });
       });
+
+      it('alternative constructor and tampered then', function() {
+        var two = Promise.resolve(2);
+        var thenCalled = 0;
+        var resolveCalled = 0;
+
+        two.then = function() {
+          thenCalled++;
+          return Promise.prototype.then.apply(this, arguments);
+        };
+
+        function AlternativeConstructor(executor) {
+          var followers = this.followers = [];
+          executor(function(value) {
+            followers.forEach(function(onFulfillment) {
+              onFulfillment(value)
+            });
+          }, function() {
+            throw TypeError('No Rejections supported');
+          });
+        }
+
+        AlternativeConstructor.resolve = function(x) {
+          resolveCalled++;
+          return new Promise(function(resolve) { resolve(x); });
+        };
+
+        AlternativeConstructor.prototype.then = function(onFulfillment, onRejection) {
+          this.followers.push(onFulfillment);
+        };
+
+        return Promise.all.call(AlternativeConstructor, [two]).then(function(value) {
+          assert.equal(thenCalled, 1);
+          assert.equal(resolveCalled, 1);
+          assert.deepEqual(value, [2]);
+        });
+      });
     });
+
+    describe('core-js species test', function() {
+      it('foreign thenable has correct internal slots', function() {
+        var p = Promise.resolve();
+
+        function NewConstructor(it) {
+          it(function(){}, function(){})
+        }
+
+        p.constructor = NewConstructor;
+
+        var f = p.then(function(){});
+        assert(f instanceof NewConstructor);
+      });
+    });
+    describe('Promise.race', function() {
+      it('tampered resolved and then', function() {
+        var two = Promise.resolve(2);
+        var thenCalled = 0;
+        var resolveCalled = 0;
+
+        two.then = function() {
+          thenCalled++;
+          return Promise.prototype.then.apply(this, arguments);
+        };
+
+        Promise.resolve = function(x) {
+          resolveCalled++;
+          return new Promise(function(resolve) { resolve(x); });
+        };
+
+        return Promise.race([two]).then(function(value) {
+          assert.equal(thenCalled, 1);
+          assert.equal(resolveCalled, 1);
+          assert.deepEqual(value, 2);
+        });
+      });
+
+      it('alternative constructor and tampered then', function() {
+        var two = Promise.resolve(2);
+        var thenCalled = 0;
+        var resolveCalled = 0;
+
+        two.then = function() {
+          thenCalled++;
+          return Promise.prototype.then.apply(this, arguments);
+        };
+
+        function AlternativeConstructor(executor) {
+          var followers = this.followers = [];
+          executor(function(value) {
+            followers.forEach(function(onFulfillment) {
+              onFulfillment(value)
+            });
+          }, function() {
+            throw TypeError('No Rejections supported');
+          });
+        }
+
+        AlternativeConstructor.resolve = function(x) {
+          resolveCalled++;
+          return new Promise(function(resolve) { resolve(x); });
+        };
+
+        AlternativeConstructor.prototype.then = function(onFulfillment, onRejection) {
+          this.followers.push(onFulfillment);
+        };
+
+        return Promise.race.call(AlternativeConstructor, [two]).then(function(value) {
+          assert.equal(thenCalled, 1);
+          assert.equal(resolveCalled, 1);
+          assert.deepEqual(value, 2);
+        });
+      });
+    });
+
   });
 });
 
